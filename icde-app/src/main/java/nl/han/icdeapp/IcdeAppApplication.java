74afc5d002ff2d8699429f75b95cfdfd6bebf8fa
Frans Geenen <fransgeenen@Franss-MacBook-Pro.local>
2019-11-27 17:56:19 +0100

    Added boilerplate STS icde-app

---
+++ icde-app/src/main/java/nl/han/icdeapp/IcdeAppApplication.java
@@ -0,0 +1,13 @@
+package nl.han.icdeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcdeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcdeAppApplication.class, args);
+	}
+
+}
---
+++ icde-app/src/test/java/nl/han/icdeapp/IcdeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.han.icdeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcdeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
